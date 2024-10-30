a4821c76f476cacc023a1ff4700a635ff359a1ea
Kosmalski Wojciech <jawmen999@gmail.com>
2024-10-30 18:23:39 +0100

    init: add coil lib

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -46,6 +46,8 @@ android {
 }
 
 dependencies {
+    implementation("io.coil-kt.coil3:coil-compose:3.0.0-rc02")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.0-rc02")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.play.services.wearable)
