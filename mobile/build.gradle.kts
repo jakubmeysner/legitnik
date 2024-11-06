059b8cf36d7c73106390f1ba5f61fba43f6cf5d7
Kosmalski Wojciech <jawmen999@gmail.com>
2024-11-06 19:43:48 +0100

    refact: add deps to catalogs

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -46,8 +46,10 @@ android {
 }
 
 dependencies {
-    implementation("io.coil-kt.coil3:coil-compose:3.0.0-rc02")
-    implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.0-rc02")
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.play.services.wearable)
