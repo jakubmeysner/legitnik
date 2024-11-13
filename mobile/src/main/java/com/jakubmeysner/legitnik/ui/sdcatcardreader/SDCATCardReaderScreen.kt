7061344e33b4e5ff88c735ac078f64f94bba6222
Jakub Meysner <jakub@jakubmeysner.com>
2024-11-13 16:21:49 +0100

    Add NFC adapter availability info text to SDCAT card reader screen

--- mobile/src/main/java/com/jakubmeysner/legitnik/ui/sdcatcardreader/SDCATCardReaderScreen.kt
+++ mobile/src/main/java/com/jakubmeysner/legitnik/ui/sdcatcardreader/SDCATCardReaderScreen.kt
@@ -1,12 +1,16 @@
 package com.jakubmeysner.legitnik.ui.sdcatcardreader
 
+import android.nfc.NfcAdapter
+import android.nfc.NfcManager
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -17,6 +21,9 @@ fun SDCATCardReaderScreen(
     viewModel: SDCATCardReaderViewModel = hiltViewModel(),
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val context = LocalContext.current
+    val nfcManager = remember(context) { context.getSystemService(NfcManager::class.java) }
+    val nfcAdapter: NfcAdapter? = remember(nfcManager) { nfcManager.defaultAdapter }
 
     Box(modifier = Modifier.padding(16.dp)) {
         Column {
@@ -26,6 +33,10 @@ fun SDCATCardReaderScreen(
             )
 
             Text(uiState.selectedInterface.name)
+
+            if (uiState.selectedInterface == SDCATCardReaderInterface.NFC) {
+                Text("NFC adapter " + if (nfcAdapter != null) "available" else "unavailable")
+            }
         }
     }
 }
