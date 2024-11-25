10f32b82013ec3a53601318ecaca02bbbc4f1e8c
Kosmalski Wojciech <jawmen999@gmail.com>
2024-11-25 19:38:49 +0100

    feat: add vico charts library

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -80,6 +80,8 @@ dependencies {
     implementation(libs.play.services.maps)
     implementation(libs.google.maps.compose)
     implementation(libs.firebase.messaging)
+    implementation(libs.vico.compose)
+    implementation(libs.vico.compose.m3)
     kapt(libs.hilt.compiler)
     kapt(libs.androidx.lifecycle.compiler)
     testImplementation(libs.junit)
