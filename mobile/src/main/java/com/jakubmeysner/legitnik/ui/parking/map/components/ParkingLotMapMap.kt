8230a4514d5b80b28fb0f105dd71c6d70100bd81
Jakub Meysner <jakub@jakubmeysner.com>
2024-12-09 01:18:07 +0100

    Add show my location option to parking lot map

--- mobile/src/main/java/com/jakubmeysner/legitnik/ui/parking/map/components/ParkingLotMapMap.kt
+++ mobile/src/main/java/com/jakubmeysner/legitnik/ui/parking/map/components/ParkingLotMapMap.kt
@@ -1,6 +1,14 @@
 package com.jakubmeysner.legitnik.ui.parking.map.components
 
+import android.Manifest
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -10,17 +18,28 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.LatLngBounds
 import com.google.maps.android.compose.ComposeMapColorScheme
+import com.google.maps.android.compose.DefaultMapProperties
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.rememberCameraPositionState
+import com.jakubmeysner.legitnik.R
 import com.jakubmeysner.legitnik.data.parking.ParkingLot
 
+@OptIn(ExperimentalPermissionsApi::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun ParkingLotMapMap(
     parkingLots: List<ParkingLot>,
@@ -36,6 +55,13 @@ fun ParkingLotMapMap(
     if (!isPlayServicesAvailable) {
         return
     }
+
+    val locationPermissionsState = rememberMultiplePermissionsState(
+        listOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+        )
+    )
 
     val cameraPositionState = rememberCameraPositionState()
     var cameraMoved by rememberSaveable { mutableStateOf(false) }
@@ -57,18 +83,46 @@ fun ParkingLotMapMap(
         }
     }
 
-    GoogleMap(
-        modifier = Modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState,
-        mapColorScheme = ComposeMapColorScheme.FOLLOW_SYSTEM,
+    Scaffold(
+        floatingActionButton = {
+            if (locationPermissionsState.permissions.none { it.status.isGranted }) {
+                Box(
+                    modifier = Modifier.padding(bottom = 96.dp),
+                ) {
+                    FloatingActionButton(
+                        onClick = {
+                            locationPermissionsState.launchMultiplePermissionRequest()
+                        }
+                    ) {
+                        Icon(
+                            imageVector = ImageVector.vectorResource(R.drawable.mdi_crosshairs_gps),
+                            contentDescription = stringResource(
+                                R.string.parking_lot_map_show_my_location
+                            ),
+                        )
+                    }
+                }
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End,
     ) {
-        for (parkingLot in parkingLots) {
-            key(parkingLot.id) {
-                ParkingLotMapMarker(
-                    parkingLot = parkingLot,
-                    navigateToParkingLotDetails = navigateToParkingLotDetails,
-                )
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState,
+            properties = DefaultMapProperties.copy(
+                isMyLocationEnabled = locationPermissionsState.permissions.any { it.status.isGranted }
+            ),
+            mapColorScheme = ComposeMapColorScheme.FOLLOW_SYSTEM,
+        ) {
+            for (parkingLot in parkingLots) {
+                key(parkingLot.id) {
+                    ParkingLotMapMarker(
+                        parkingLot = parkingLot,
+                        navigateToParkingLotDetails = navigateToParkingLotDetails,
+                    )
+                }
             }
         }
     }
+
 }
