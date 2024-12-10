019fc72a2d6a693cb9baebba90c41ea3ef1d8697
Vixoner <vixonerpl@gmail.com>
2024-12-10 14:21:12 +0100

    Made a settings page with persisting settings

--- mobile/src/main/java/com/jakubmeysner/legitnik/ui/settings/SettingsScreen.kt
+++ mobile/src/main/java/com/jakubmeysner/legitnik/ui/settings/SettingsScreen.kt
@@ -1,12 +1,140 @@
 package com.jakubmeysner.legitnik.ui.settings
 
+import androidx.compose.foundation.background
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.jakubmeysner.legitnik.R
 
 @Composable
-fun SettingsScreen(
-    viewModel: SettingsViewModel = hiltViewModel(),
+fun SettingsScreen(viewModel: SettingsViewModel = hiltViewModel()) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val parkingLots = uiState.parkingLots
+
+    var parkingLabels = listOf("WRO", "C13", "D20", "GEO-L", "E01") // idk czy to dobry pomysl? Bo w sumie jak często pojawiają się nowe parkingi na pwr? A takto jest dostęp do strony ustawień nawet jeśli nie ma połączenia. Thoughts?
+
+    if (parkingLots != null) {
+        parkingLabels = parkingLots.map { it.symbol }
+    }
+
+    val isNotificationCategoryEnabled by viewModel.isCategoryEnabled(SettingCategory.NOTIFICATION).collectAsState(false)
+    val isTrackingCategoryEnabled by viewModel.isCategoryEnabled(SettingCategory.ONGOING).collectAsState(false)
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        item {
+            SettingsCategory(
+                title = stringResource(R.string.settings_notification_category),
+                isEnabled = isNotificationCategoryEnabled,
+                onToggle = { newValue -> viewModel.toggleCategory(newValue, SettingCategory.NOTIFICATION) },
+                parkingLabels = parkingLabels,
+                viewModel = viewModel,
+                category = SettingCategory.NOTIFICATION
+            )
+        }
+
+        item {
+            HorizontalDivider(
+                color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.2f),
+                thickness = 1.dp
+            )
+        }
+
+        item {
+            SettingsCategory(
+                title = stringResource(R.string.settings_ongoing_notification_category),
+                isEnabled = isTrackingCategoryEnabled,
+                onToggle = { newValue -> viewModel.toggleCategory(newValue, SettingCategory.ONGOING) },
+                parkingLabels = parkingLabels,
+                viewModel = viewModel,
+                category = SettingCategory.ONGOING
+            )
+        }
+    }
+}
+
+@Composable
+fun SettingsCategory(
+    title: String,
+    isEnabled: Boolean,
+    onToggle: (Boolean) -> Unit,
+    parkingLabels: List<String>,
+    viewModel: SettingsViewModel,
+    category: SettingCategory
 ) {
-    Text("Settings")
+    Column {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .padding(horizontal = 4.dp, vertical = 8.dp),
+        ) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.weight(1f)
+            )
+            Switch(
+                checked = isEnabled,
+                onCheckedChange = onToggle
+            )
+        }
+        if (isEnabled) {
+            parkingLabels.forEachIndexed { index, label ->
+                ToggleItem(
+                    viewModel = viewModel,
+                    label = label,
+                    category = category,
+                    index = index
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ToggleItem(
+    viewModel: SettingsViewModel,
+    label: String,
+    category: SettingCategory,
+    index: Int
+) {
+    // używam tu null, bo jeżeli dam initial state na np. false, to za każdym przełączeniem kategorii w ustawieniach te switche odpalają animację przełączenia.
+    // idk czy to dobry pomysl
+    val isCheckedFromViewModel by viewModel.isSettingEnabled(label, category).collectAsState(initial = null)
+
+    if (isCheckedFromViewModel != null) {
+        Row(
+            modifier = Modifier
+                .background(
+                    if (index % 2 == 0) MaterialTheme.colorScheme.surfaceTint.copy(alpha = 0.05f)
+                    else MaterialTheme.colorScheme.background
+                )
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = label,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Switch(
+                checked = isCheckedFromViewModel!!,
+                onCheckedChange = { newValue ->
+                    viewModel.toggleSetting(label, newValue, category)
+                }
+            )
+        }
+    }
 }
--- mobile/src/main/java/com/jakubmeysner/legitnik/ui/settings/SettingsViewModel.kt
+++ mobile/src/main/java/com/jakubmeysner/legitnik/ui/settings/SettingsViewModel.kt
@@ -1,11 +1,97 @@
 package com.jakubmeysner.legitnik.ui.settings
 
+import android.content.Context
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jakubmeysner.legitnik.ui.parking.list.ParkingLotUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+import com.jakubmeysner.legitnik.data.parking.ParkingLotRepository
+
+val Context.dataStore by preferencesDataStore(name = "settings")
+
+enum class SettingCategory {
+    NOTIFICATION,
+    ONGOING
+}
 
 @HiltViewModel
 class SettingsViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle,
-) : ViewModel()
+    @ApplicationContext private val context: Context,
+    private val parkingLotRepository: ParkingLotRepository
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(ParkingLotUiState())
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        loadParkingLots()
+    }
+
+    private fun loadParkingLots(forceRefresh: Boolean = false) {
+        viewModelScope.launch {
+            try {
+                _uiState.update { currentUiState ->
+                    currentUiState.copy(loading = true, error = false)
+                }
+                val parkingLots = parkingLotRepository.getParkingLots(forceRefresh)
+                _uiState.update { currentUiState ->
+                    currentUiState.copy(
+                        loading = false,
+                        parkingLots = parkingLots,
+                        error = false
+                    )
+                }
+            } catch (e: Exception) {
+                _uiState.update { currentUiState ->
+                    currentUiState.copy(
+                        error = true,
+                        loading = false
+                    )
+                }
+            }
+        }
+    }
+
+    private fun categoryKey(category: SettingCategory) =
+        booleanPreferencesKey("${category.name.lowercase()}_category_enabled")
+
+    private fun settingKey(label: String, category: SettingCategory) =
+        booleanPreferencesKey("${category.name.lowercase()}_$label")
+
+    fun isCategoryEnabled(category: SettingCategory): Flow<Boolean> {
+        return context.dataStore.data
+            .map { preferences -> preferences[categoryKey(category)] ?: false }
+            .distinctUntilChanged()
+    }
+
+    fun toggleCategory(isEnabled: Boolean, category: SettingCategory) {
+        viewModelScope.launch {
+            context.dataStore.edit { preferences ->
+                preferences[categoryKey(category)] = isEnabled
+            }
+        }
+    }
+
+    fun isSettingEnabled(label: String, category: SettingCategory): Flow<Boolean> {
+        return context.dataStore.data
+            .map { preferences -> preferences[settingKey(label, category)] ?: false }
+            .distinctUntilChanged()
+    }
+
+    fun toggleSetting(label: String, isEnabled: Boolean, category: SettingCategory) {
+        viewModelScope.launch {
+            context.dataStore.edit { preferences ->
+                preferences[settingKey(label, category)] = isEnabled
+            }
+        }
+    }
+}
+
