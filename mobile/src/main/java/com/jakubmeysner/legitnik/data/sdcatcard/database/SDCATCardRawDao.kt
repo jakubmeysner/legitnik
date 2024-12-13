a457440630ba88792e77fa3b60824d0606c81e91
Kosmalski Wojciech <jawmen999@gmail.com>
2024-12-13 19:24:57 +0100

    refact: change getOne DAO method return type to nullable

--- mobile/src/main/java/com/jakubmeysner/legitnik/data/sdcatcard/database/SDCATCardRawDao.kt
+++ mobile/src/main/java/com/jakubmeysner/legitnik/data/sdcatcard/database/SDCATCardRawDao.kt
@@ -12,7 +12,7 @@ interface SDCATCardRawDao {
     suspend fun getAll(): List<SDCATCardRawDataEntity>
 
     @Query("SELECT * FROM SDCAT_card_raw_data WHERE uuid = :uuid")
-    suspend fun getOne(uuid: UUID): SDCATCardRawDataEntity
+    suspend fun getOne(uuid: UUID): SDCATCardRawDataEntity?
 
     @Query("SELECT * FROM SDCAT_card_raw_data WHERE hash = :hash")
     suspend fun getOneByHash(hash: List<Byte>): SDCATCardRawDataEntity?
