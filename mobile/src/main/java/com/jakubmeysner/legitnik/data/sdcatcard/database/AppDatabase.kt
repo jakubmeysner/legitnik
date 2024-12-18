fddc01ce10f3aba116c9cd6b60a96cfe8bf27a2d
Jakub Meysner <jakub@jakubmeysner.com>
2024-12-18 13:35:49 +0100

    Add default field to SDCATCardRawDataEntity

--- mobile/src/main/java/com/jakubmeysner/legitnik/data/sdcatcard/database/AppDatabase.kt
+++ mobile/src/main/java/com/jakubmeysner/legitnik/data/sdcatcard/database/AppDatabase.kt
@@ -1,10 +1,17 @@
 package com.jakubmeysner.legitnik.data.sdcatcard.database
 
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [SDCATCardRawDataEntity::class], version = 1)
+@Database(
+    version = 2,
+    entities = [SDCATCardRawDataEntity::class],
+    autoMigrations = [
+        AutoMigration(from = 1, to = 2),
+    ],
+)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun sdcatCardRawDao(): SDCATCardRawDao
