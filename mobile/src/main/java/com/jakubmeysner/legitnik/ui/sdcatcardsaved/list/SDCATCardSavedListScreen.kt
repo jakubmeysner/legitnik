de6e649ec4d9b25aba4e491435a47fa74b8b991a
Jakub Meysner <jakub@jakubmeysner.com>
2024-12-18 13:41:43 +0100

    Add SDCAT card saved list screen impl

--- mobile/src/main/java/com/jakubmeysner/legitnik/ui/sdcatcardsaved/list/SDCATCardSavedListScreen.kt
+++ mobile/src/main/java/com/jakubmeysner/legitnik/ui/sdcatcardsaved/list/SDCATCardSavedListScreen.kt
@@ -1,12 +1,55 @@
 package com.jakubmeysner.legitnik.ui.sdcatcardsaved.list
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.jakubmeysner.legitnik.R
+import com.jakubmeysner.legitnik.ui.components.FullScreenPrompt
+import com.jakubmeysner.legitnik.ui.sdcatcardsaved.list.components.SDCATCardSavedListCard
 
 @Composable
 fun SDCATCardSavedListScreen(
     viewModel: SDCATCardSavedListViewModel = hiltViewModel(),
 ) {
-    Text(text = "SDCATCardSavedListScreen")
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val cards = uiState.cards
+
+    if (cards != null) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(all = 16.dp)
+        ) {
+            if (cards.isNotEmpty()) {
+                LazyColumn(
+                    verticalArrangement = Arrangement.spacedBy(space = 16.dp),
+                ) {
+                    for (data in cards) {
+                        item(data.rawData.id) {
+                            SDCATCardSavedListCard(
+                                data = data,
+                            )
+                        }
+                    }
+                }
+            } else {
+                FullScreenPrompt(
+                    icon = ImageVector.vectorResource(R.drawable.mdi_playlist_plus),
+                    title = stringResource(R.string.sdcat_card_saved_list_prompt_title),
+                    subtitle = stringResource(R.string.sdcat_card_saved_list_prompt_subtitle),
+                )
+            }
+        }
+    }
 }
---
+++ mobile/src/main/java/com/jakubmeysner/legitnik/ui/sdcatcardsaved/list/components/SDCATCardSavedListCard.kt
@@ -0,0 +1,84 @@
+package com.jakubmeysner.legitnik.ui.sdcatcardsaved.list.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import com.jakubmeysner.legitnik.R
+import com.jakubmeysner.legitnik.data.sdcatcard.SDCATCardDataEntity
+import com.jakubmeysner.legitnik.data.sdcatcard.SDCATCardParsedContent
+
+@Composable
+fun SDCATCardSavedListCard(
+    data: SDCATCardDataEntity,
+) {
+    val cardColors = if (data.rawData.default == true) {
+        CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primaryContainer)
+    } else {
+        CardDefaults.cardColors()
+    }
+
+    val fullName = "${data.parsedData.content.givenNames.joinToString(separator = " ")} ${
+        data.parsedData.content.surname.joinToString(separator = " ")
+    }"
+
+    val type = stringResource(
+        when (data.parsedData.content) {
+            is SDCATCardParsedContent.StudentCardParsedContent -> R.string.sdcat_card_card_student
+            is SDCATCardParsedContent.DoctoralCandidateCardParsedContent -> R.string.sdcat_card_card_doctoral_candidate
+            is SDCATCardParsedContent.AcademicTeacherCardParsedContent -> R.string.sdcat_card_card_academic_teacher
+        }
+    )
+
+    val subtitle = "$type (${data.parsedData.content.universityOrIssuerName})"
+
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        colors = cardColors,
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp, vertical = 24.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(space = 4.dp),
+            ) {
+                Text(
+                    text = fullName,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+
+                Text(
+                    text = subtitle,
+                    style = MaterialTheme.typography.labelLarge,
+                )
+            }
+
+            if (data.rawData.default == true) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.contactless_payment_circle),
+                    contentDescription = "Default",
+                    modifier = Modifier.size(size = 32.dp),
+                )
+            }
+        }
+    }
+}
