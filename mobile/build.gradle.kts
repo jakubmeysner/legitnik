a92c89d3e7b766e9092b0f4a0612c6be499d3d26
Kosmalski Wojciech <jawmen999@gmail.com>
2025-01-21 19:57:35 +0100

    feat: add hilt-android-test dependency

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -116,6 +116,8 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+    androidTestImplementation(libs.hilt.testing)
+    kaptAndroidTest(libs.hilt.compiler)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(libs.datastore.protobuf)
